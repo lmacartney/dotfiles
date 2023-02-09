7e740d2d62556cc835ff269a7b5d7f61bd0f3080
Lachlan Macartney <lmacartney@canva.com>
2023-02-09 11:36:19 +1100

    delete kaz:

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,1 @@
-local ok, impatient = pcall(require, 'impatient')
-if ok then
-  impatient.enable_profile()
-else
-  vim.notify(impatient)
-end
-
-require('kaz.settings')
-require('kaz.keybindings')
-require('kaz.plugins')
-require('kaz.autocmds')
-require('kaz.commands')
+require('lachstr')
--- .config/nvim/lua/kaz/plugins/vim-illuminate.lua
+++
@@ -1,2 +0,0 @@
-vim.g.Illuminate_delay = 300
-vim.g.Illuminate_highlightUnderCursor = 0
